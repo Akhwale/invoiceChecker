77c67bf28a6ecd0413da83a5c49458390c2a8ecf
Edwin Akhwale <edwin.akhwale@aln.africa>
2024-05-23 23:50:38 +0300

    Front end structure

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,34 @@
 const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const fs = require("fs");
+const { createWorker } = require("tesseract.js");
+
+const upload = multer({ dest: 'uploads/' });
 
 const app = express();
+app.use(cors());
 
 const PORT = 5000;
+
+
+app.get('/', (req,res) => {
+    res.send("Hello world")
+});
+
+
+app.get('/upload', (req, res) => {
+    if (!req.file) {
+      return res.status(400).send('No file uploaded');
+      console.log("no vile man");
+    }
+
+    // Check if uploaded file is a PDF
+    if (req.file.mimetype !== 'application/pdf') {
+        return res.status(400).send('Only PDF files are allowed');
+    }
+
+        }
+);
 
 app.listen(PORT, () => console.log(`you are listening to port ${PORT}`));
---
+++ src/components/Home.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]); // Get the first selected file
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent default form submission
+
+    if (!file) {
+      alert('Please select a file first');
+      return;
+    }
+
+    // Create a FormData object
+    const formData = new FormData();
+    // Append the selected file to the FormData object
+    formData.append('file', file);
+
+    try {
+      // Send a POST request with the form data to the server
+      const response = await axios.post('http://localhost:3000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      alert('File uploaded successfully');
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      alert('Failed to upload file');
+    }
+  };
+
+  return (
+    <>
+      Home ground manze!
+
+      <form onSubmit={handleSubmit}>
+        <input type="file" name="doc" onChange={handleFileChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
